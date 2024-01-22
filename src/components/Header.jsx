1583e6a017fdf98b43b28a01b4620c28151b2746
boryana.yordanova <boryana.yordanova@flatrocktech.com>
2024-01-22 18:07:06 +0200

    modal added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,29 @@
 import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button.jsx';
+import { useContext } from 'react';
+import CartContext from '../store/CartContext.jsx';
+import UserProgressContext  from "../store/UserProgressContext.jsx";
+
 
 export default function Header() {
+    const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
+
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity;
+    }, 0);
+
+    function handleShowCart() {
+        userProgressCtx.showCart();
+    }
+
     return <header id="main-header">
         <div id="title">
             <img src={logoImg} alt="A restaurant" />
             <h1>React Food</h1>
         </div>
         <nav>
-            <Button textOnly>Cart (0)</Button>
+            <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
         </nav>
         </header>
 }
--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,16 +1,28 @@
 import { currencyFormatter } from "../util/formatting.js";
 import Button from "./UI/Button.jsx";
+import CartContext from '../store/CartContext.jsx';
+import { useContext } from "react";
+
+
 export default function MealItem({meal}){
+
+    const cartCtx = useContext(CartContext);
+    
+    function handleAddMealToCart() {
+        cartCtx.addItem(meal);
+    }
+
+
     return <li className="meal-item">
         <article>
             <img src={`http://localhost:3000/${meal.image}`} alt={meal.name}></img>
             <div>
                 <h3>{meal.name}</h3>
                 <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
-                <p className="meal-item-descrpiption">{meal.description}</p>
+                <p className="meal-item-description">{meal.description}</p>
             </div>
             <p className="meal-item-actions">
-                <Button>Add to Card</Button>
+                <Button onClick={handleAddMealToCart}>Add to Card</Button>
             </p>
         </article>
     </li>
